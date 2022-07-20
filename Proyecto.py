ab926e4908d5ca190a93aa64c2d7bfbe3737874b
rsanhueza81 <67172949+rsanhueza81@users.noreply.github.com>
2022-07-20 17:24:49 -0400

    Update Proyecto.py

--- Proyecto.py
+++ Proyecto.py
@@ -104,5 +104,5 @@ st.title("Visualización preliminar proyecto")
 stripplot | legend
 
 
-st.altair_chart(stripplot.interactive(), use_container_width=True)
+st.altair_chart(stripplot, use_container_width=False)
 
