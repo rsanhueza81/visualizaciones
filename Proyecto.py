70a39e7b003ed18dc727dd3174e0cc7bd980b72a
gui-rub <81867895+gui-rub@users.noreply.github.com>
2022-07-21 00:36:10 -0400

    Update Proyecto.py

--- Proyecto.py
+++ Proyecto.py
@@ -11,64 +11,128 @@ import streamlit as st
 
 
 alt.data_transformers.disable_max_rows()
+
+#####################################################################
+############### LECTURA Y TRANSFORMACION DE DATOS ###################
+#####################################################################
 data = pd.read_csv('almagro_cot.csv')
 
 data1 = data.copy()
 data1 = data1[data1['Tipo Unidad'] =='Departamento']
-data1 = data[['N° Dormitorios','N° Baños','Comuna Proyecto','Precio de Venta en Uf Cotizacion','Metros Cuadrados','Estado Civil','Comuna Cliente', 'Edad', 'Programa']]
-data1.columns = ['n_dormitorios', 'n_baños', 'comuna_proy', 'precio','m2','estado_civil','comuna_cli', 'edad','programa']
+data1 = data1[['N° Dormitorios','N° Baños','Comuna Proyecto','Precio de Venta en Uf Cotizacion','Metros Cuadrados','Estado Civil','Comuna Cliente', 'Edad', 'Programa','Fecha Reserva','Nombre Proyecto']]
+data1.columns = ['n_dormitorios', 'n_baños', 'comuna_proy', 'precio','m2','estado_civil','comuna_cli', 'edad','programa','fecha_reserva','nombre_proyecto']
+
+data1['n_dormitorios'] = data1.n_dormitorios.astype(int).astype(str)
+data1['n_baños'] = data1.n_baños.astype(int).astype(str)
+
+data1['reservado'] = 'No'
+data1.loc[data1.fecha_reserva.notna(), 'reservado'] = 'Sí'
+del data1['fecha_reserva']
+
 data1 = data1[data1.isna().sum(axis=1)==0]
 data1['uf_m2'] = data1.precio/data1.m2
-data1 = data1[data1.comuna_cli.isin(data1.comuna_cli.value_counts()[:10].index)]
 data1 = data1[data1.uf_m2<200] 
-data1 = data1[data1.estado_civil!='Empresa']
+
 data1.loc[data1.programa.isin(['TRADICIONAL KC']), 'programa'] = 'Tradicional KC'
 data1.loc[data1.programa.isin(['TRADICIONAL KA']), 'programa'] = 'Tradicional KA'
 data1.loc[data1.programa.isin(['PENTHOUSE 2 PISOS','PENTHOUSE  2 PISOS','Penthouse 2']), 'programa'] = 'Penthouse 2 pisos'
 data1.loc[data1.programa.isin(['Palomita']), 'programa'] = 'Paloma'
 data1 = data1[data1.programa.isin(data1['programa'].value_counts()[:8].index)]
-data1.loc[data1.estado_civil.str.contains('Casado'), 'estado_civil'] = 'Casado(a)'
-data1.loc[data1.estado_civil.str.contains('Union Civil'), 'estado_civil'] = 'Casado(a)'
-data1.loc[data1.estado_civil.str.contains('Soltero'), 'estado_civil'] = 'Soltero(a)'
-data1.loc[data1.estado_civil.str.contains('Separado'), 'estado_civil'] = 'Divorciado(a)'
-data1.loc[data1.estado_civil.str.contains('0'), 'estado_civil'] = 'Sin información'
-selection = alt.selection_multi(fields=['programa', 'n_dormitorios'])
+#####################################################################
+######################## VISUALIZACION 1 ############################
+#####################################################################
+selection = alt.selection_single(fields=['reservado'])
+
 color = alt.condition(selection,
-                      alt.Color('programa:N', legend=None),
+                      alt.Color('reservado:N', legend=None, scale=alt.Scale(scheme='paired')),
                       alt.value('lightgray'))
 
-stripplot =  (alt.Chart(data1, width=40).mark_circle(size=8).encode(
+stripplot =  alt.Chart(data1, width=40).mark_point().encode(
     x=alt.X(
         'jitter:Q',
         title=None,
         axis=alt.Axis(values=[0], ticks=True, grid=False, labels=False),
         scale=alt.Scale(),
     ),
-    y=alt.Y('uf_m2:Q'),
+    y=alt.Y('uf_m2:Q', title='UF por m2'),
     color=color,
+    tooltip = 'nombre_proyecto',
     column=alt.Column(
         'comuna_proy:N',
         header=alt.Header(
             labelAngle=-90,
-            titleOrient='top',
+            title = 'Comuna proyecto',
+            titleOrient='bottom',
             labelOrient='bottom',
-            labelAlign='right',
-            labelPadding=3,
+            labelAlign='left',
+            labelPadding=300-3
         ),
     ),
 ).transform_calculate(
     # Generate Gaussian jitter with a Box-Muller transform
     jitter='sqrt(-2*log(random()))*cos(2*PI*random())'
-).interactive())
+)
 
-legend = alt.Chart(data1).mark_rect().encode(
-    y=alt.Y('programa:N', axis=alt.Axis(orient='right')),
-    x='n_dormitorios:O',
+stripplot = stripplot.properties(title=alt.TitleParams(
+    text="UF por m2 aperturado por comuna del proyecto",
+    subtitle=["Inmobiliaria Almagro",""],
+    fontSize=20,
+    color='#3169A4',
+    subtitleFontSize=16,
+    subtitleColor="#525252",
+    anchor="start"))
+
+
+legend = alt.Chart(data1).mark_bar().encode(
+    y=alt.Y('count()', title='Cantidad cotizaciones'),
+    x=alt.X('reservado:O', title='Reservado'),
     color=color
 ).add_selection(
     selection
-).interactive()
+)
 
+programa_dropdown = alt.binding_select(options=[None] + list(data1.programa.unique()), labels = ['All'] + list(data1.programa.unique()), name="Programa")
+programa_select = alt.selection_single(fields=['programa'], bind=programa_dropdown)
+
+dormitorio_slider = alt.binding_select(options=[None] + list(sorted(data1.n_dormitorios.unique())), labels = ['All'] + list(sorted(data1.n_dormitorios.unique())), name="Dormitorios")
+dormitorio_select = alt.selection_single(fields=['n_dormitorios'], bind=dormitorio_slider)
+
+baño_slider = alt.binding_select(options=[None] + list(sorted(data1.n_baños.unique())), labels = ['All'] + list(sorted(data1.n_baños.unique())), name="Baños")
+baño_select = alt.selection_single(fields=['n_baños'], bind=baño_slider)
+
+
+stripplot = stripplot.add_selection(
+    baño_select
+).transform_filter(
+    baño_select
+).add_selection(
+    dormitorio_select
+).transform_filter(
+    dormitorio_select
+).add_selection(
+    programa_select
+).transform_filter(
+    programa_select
+)
+
+
+legend = legend.add_selection(
+    baño_select
+).transform_filter(
+    baño_select
+).add_selection(
+    dormitorio_select
+).transform_filter(
+    dormitorio_select
+).add_selection(
+    programa_select
+).transform_filter(
+    programa_select
+)
+
+################################################################################
+############################## HTML ############################################
+################################################################################
 st.title("Visualización preliminar proyecto")
 
 #row1_1, row1_2 = st.columns((2, 3))
