68b115f03604916d596a372c25cdd34e106436ae
gui-rub <81867895+gui-rub@users.noreply.github.com>
2022-07-30 18:10:32 -0400

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -75,7 +75,7 @@ st.subheader('Prototipo de Idiom:')
 data_v1=data1.sample(6000,random_state=3).copy()
 slider_banos = st.slider('Número de baños',1, 4, (1,4),step=1)
 
-data_v1=data_v1[data_v1['n_banos']>=min_banos]
+data_v1=data_v1[data_v1['n_banos']>=slider_banos[0]]
 
 selection = alt.selection_single(fields=['reservado'])
 
