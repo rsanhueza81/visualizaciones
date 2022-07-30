b699a766edb117760205e84399e61f7bbb51c44f
gui-rub <81867895+gui-rub@users.noreply.github.com>
2022-07-30 17:43:55 -0400

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -161,7 +161,7 @@ Se destacan cuatro familias de variables:
 
 st.subheader('Prototipo de Idiom:')
 
-values = st.slider(     'Select a range of values',data1.n_baños.min(), data1.n_baños.max(), (data1.n_baños.min(), data1.n_baños.max()),step=1)
-st.write('Values:', values)
+#values = st.slider(     'Select a range of values',data1.n_baños.min(), data1.n_baños.max(), (data1.n_baños.min(), data1.n_baños.max()),step=1)
+st.write('Values:', data1.n_baños.min())
 
 stripplot | legend
