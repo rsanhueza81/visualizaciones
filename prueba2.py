8a2cf42e31e05346c6f5a524da7da7cfc631935d
rsanhueza81 <67172949+rsanhueza81@users.noreply.github.com>
2022-07-30 18:47:19 -0400

    Update prueba2.py

--- prueba2.py
+++ prueba2.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 from PIL import Image
 
-mport altair as alt
+import altair as alt
 import pandas as pd
 import numpy as np
 from altair.expr import datum
