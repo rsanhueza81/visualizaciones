d3e1eecfb9bccf00f52721aa988b95fa5b0fc04e
rsanhueza81 <67172949+rsanhueza81@users.noreply.github.com>
2022-07-30 17:37:04 -0400

    Update prueba2.py

--- prueba2.py
+++ prueba2.py
@@ -1,2 +1,14 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
+
+with st.sidebar:
+    choose = option_menu("App Gallery", ["About", "Photo Editing", "Project Planning", "Python e-Course", "Contact"],
+                         icons=['house', 'camera fill', 'kanban', 'book','person lines fill'],
+                         menu_icon="app-indicator", default_index=0,
+                         styles={
+        "container": {"padding": "5!important", "background-color": "#fafafa"},
+        "icon": {"color": "orange", "font-size": "25px"}, 
+        "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+        "nav-link-selected": {"background-color": "#02ab21"},
+    }
+    )
